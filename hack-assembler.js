6db1ba1f9f47cd8f0253a7cf2bceb5effeb0308a
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-11-11 12:28:57 +0800

    fix file i/o

--- hack-assembler.js
+++ hack-assembler.js
@@ -1,3 +1,30 @@
 // C - instruction 
 // A - instruction
-// Symbols: predefined, labels, variables
+// { } Symbols: predefined, labels, variables
+
+// Read file line by line --> translate to C and A
+
+// Parsing: ignore whitespace and comments //
+const readline = require('readline');
+const fs = require('fs');
+const file = process.argv[2];
+
+const rl = readline.createInterface({
+    input: fs.createReadStream(file),
+    output: process.stdout,
+    terminal: false
+});
+
+const out = file
+    .split('/')
+    .find((file) => file.includes('.asm'))
+    .replace(/.asm/, '.hack');
+
+fd = fs.openSync(out, 'w');
+
+rl.on('line', (input) => {
+    // console.log(`Received: ${input}`);
+    fs.write(fd, input + '\n', () => {
+        // console.log('object');
+    });
+});
